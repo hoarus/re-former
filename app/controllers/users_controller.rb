1aae292be8bb51a50758fcfc3e08e733e7781887
hoarus <hoarus@gmail.com>
2021-12-06 20:44:48 +1000

    Implement rails helper for new and edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    # @users = User.all
+  end
+ 
   def new
+    @user = User.new
   end
 
   def create
@@ -12,9 +17,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :id)
   end
 
 end
